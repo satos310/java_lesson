ea39cd27979aa78b0e4a9a630794c559dda55d5e
satos310 <119788681+satos310@users.noreply.github.com>
2023-08-23 10:08:52 +0000

    [study]println, variable

--- study2/numberGuess.java
+++ study2/numberGuess.java
@@ -1,5 +1,8 @@
 public class numberGuess {
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) {      // Javaを実行するにはclassとmainメソッドが必要
+    String name = "Shirata";      // 変数に値を割り当てる → 「変数に値を代入する」
+  
+    System.out.println("hello " + name);
+    System.out.println("hello " + name + " again!");
   }
 }

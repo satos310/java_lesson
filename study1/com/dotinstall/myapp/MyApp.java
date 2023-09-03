b69118e08d012f96ffe39ff2b7587805e86e5062
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-03 09:51:48 +0000

    [study]this, getter, setter → アクセスコントロール

--- study1/com/dotinstall/myapp/MyApp.java
+++ study1/com/dotinstall/myapp/MyApp.java
@@ -5,8 +5,10 @@ import com.dotinstall.myapp.model.AdminUser;
 
 public class MyApp {
   public static void main(String[] args) {
-    User tom = new User("tom");     // newで新しい番地確保→ 「インスタンス化」, この状況ではtomのことを「インスタンス」と呼ぶ
-    // System.out.println(tom.name);
+    User tom = new User("tom", 65);     // newで新しい番地確保→ 「インスタンス化」, この状況ではtomのことを「インスタンス」と呼ぶ
+    tom.setScore(85);
+    tom.setScore(-22);
+    System.out.println(tom.getScore());
     tom.sayHi();
 
     AdminUser bob = new AdminUser("bob");     // 引数なしで呼ぶ　→　Me!が入る,  クラスの宣言とインスタンス化は1行で書くことができる
@@ -14,6 +16,16 @@ public class MyApp {
     bob.sayHi();
     bob.sayHello();
   }
+
+  // メソッド
+  // public static void sayHi(String name) {     // ここでsayHi()内に変数を宣言 → 上の記述で呼び出す時にsayHi()と記述すれば、任意の値を渡せる
+    // System.out.println("Hi!");
+    // System.out.println("Hi! " + name);
+
+  // メソッドの値を返してもらいたい時 → 「return 返したい値」
+  // public static String sayHi(String name) {     //  メソッドが値を返す時は、メソッド名の前に返すデータ型を書く必要有（※void → 「何も返さない」）
+  //   return "Hi! " + name;     // 引数　個数や方が違えば、同じ名前でいくつでも作ることができる（メソッドのオーバーロード）
+  // }
 
 // orverload
 // public static void sayHi() {
--- study1/com/dotinstall/myapp/model/User.java
+++ study1/com/dotinstall/myapp/model/User.java
@@ -1,11 +1,24 @@
 package com.dotinstall.myapp.model;
 
 public class User {      // class名の1文字目は必ず大文字
-  protected String name = "Me!";      // classに属するnameを「フィールド」という, tom.nameに代入される
+  // protected String name = "Me!";      // classに属するnameを「フィールド」という, tom.nameに代入される
+  private String name;
+  private int score;
 
   // constructor  クラスがインスタンス化される時に必ず呼ばれる特殊なメソッド　　メソッド==オーバーロード可
-  public User(String name) {
+  public User(String name, int score) {
     this.name = name;     // メソッド内からクラスのフィールドにアクセスするには「this」を使用　今回は"Tom"が入る
+    this.score = score;
+  }
+  
+  public int getScore() {     // getter
+    return this.score;
+  }
+  
+  public void setScore(int score) {     // setter
+    if (score > 0) {
+      this.score = score;
+    }
   }
 
   // User() {      // 引数なしでインスタンス化されたとき

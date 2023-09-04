bd39dd0800fce0b01ac9cf2288cadfdc3cd5cac6
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-04 10:57:40 +0000

    [study]final修飾子, 定数, final static

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,39 +1,36 @@
+// final: 変更ができない
 
-// staticキーワード　→　インスタンス化しなくてもクラスから直接使えるフィールドやメソッドを定義可
-class User {      // class名の1文字目は必ず大文字
-  // protected String name = "Me!";      // classに属するnameを「フィールド」という, tom.nameに代入される
-  private String name;
-  private static int count;   // 「クラス変数」　staticをつける → フィールドやメソッドをclassに紐づいた形で値を保存できる
+final class User {      // このclassは継承できなくなる
+  protected String name;
+  private static final double VERSION = 1.1;      // 定数(VERSION)にする場合にfinalを使う, staticとfinalを併せてつかうことで一意性を持たせた変数を作れる。
 
-  static {      // staticイニシャライザ　クラスを使う前に初期化処理できる
-    User.count = 0;     // クラスが初期化される際に行う処理
-    System.out.println("Static initializer");
-  }
-
-  {     // 「インスタンスイニシャライザ」　インスタンスを初期化する際に行う処理ができる（インスタンス化される前実行）
-    System.out.println("Instance initializer");
-  }
-
-  // constructor  クラスがインスタンス化される時に必ず呼ばれる特殊なメソッド（インスタンス化された後実行）　　メソッド==オーバーロード可
   public User(String name) {
-    this.name = name;     // メソッド内からクラスのフィールドにアクセスするには「this」を使用　今回は"Tom"が入る
-    User.count++;
-    System.out.println("Constructor");
+    this.name = name;
+    User.VERSION = 1.2;
   }
 
-  public static void getInfo() {      // 「クラスメソッド」  static
-    System.out.println("# of instances: " + User.count);
+  public final void sayHi() {     // このメソッドはorverrideできなくなる
+    System.out.println("hi! " + this.name);
+  }
+}
+
+class AdminUser extends User {
+
+  public AdminUser(String name) {
+    super(name);
+  }
+
+  @Override
+  public void sayHi() {
+    System.out.println("[admin] hi! " + this.name);
   }
 
 }
 
 public class MyApp {
-  public static void main(String[] args) {      // ずっとmainにもstaticがついていて、クラスメソッドになっている
-    User.getInfo();     // 0                       why... Javaの仮想マシンがMyAppのインスタンスを作らずに実行できるように
+
+  public static void main(String[] args) {
     User tom = new User("tom");
-    User.getInfo();     // 1
-    User bob = new User("bob");
-    User.getInfo();     // 2
   }
 
 }

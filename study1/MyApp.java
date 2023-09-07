c2091e55a28b38aba11e560d8444e0df0492d971
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-07 22:48:52 +0000

    [study]wrapper class, 参照型クラス, auto boxing, auto unboxing

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,31 +1,20 @@
-// 例外   // プログラムの実行中に予期しない結果が発生した時の処理
+/*
+Wrapper Class   基本データ型に対応する参照型クラス
+int -> Integer
+double -> Double
+*/
 
-class MyException extends Exception {     // bが例外の時用の設定
-  public MyException(String s) {
-    super(s);
-  }
-}
 
 public class MyApp {
   
-  public static void div(int a, int b) {
-    try {     // 例外が発生しそうな箇所をtryで囲む
-    if (b < 0) {
-      throw new MyException("not minus!");      // if文がyes -> コンストラクタを投げる
-    }
-      System.out.println(a / b);
-    } catch(ArithmeticException e) {      // 例外をcatch    // catch()内に例外のワード + e を入れる
-      System.err.println(e.getMessage());     // errとすることで、標準 -> エラー出力する
-    } catch(MyException e) {
-      System.err.println(e.getMessage());
-    } finally {     // 例外の有無に限らず、最後に後処理を設定するブロック
-      System.err.println(" -- ebd --");
-    }
-  }
-  
   public static void main(String[] args) {
-    div(3, 0);
-    div(5, -2);
+    // Integer i = new Integer(32);      // Integerのラッパークラス型で i を宣言
+    // int n = i.intValue();     // ラッパークラスの値を基本データ型として取り出す
+    
+    Integer i = 32;     // auto boxing    自動変換を参照型に入れる
+    i = null;     // 参照型のデータには値そのものではなく、値が入っているメモリ領域の場所が入る -> どの場所も指示していない状態(null)
+    int n = i;      // auto unboxing    iはnullのため、unboxingできない
+    System.out.println();
   }
 
 }

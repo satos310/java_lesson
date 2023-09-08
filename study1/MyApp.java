44ddd4a1dd5ef45585c2805e988b0c443e7e9fba
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-08 07:49:12 +0000

    [study]thread, スレッド, runnable, implements・extends, start()・run()

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,33 +1,25 @@
-// generics   汎用化されたデータ型でクラスやインターフェースを作ることができる
+// Thread   コンピュータの処理単位のこと    複数立ち上げれば複数の処理を同時に実行可
 
-// class MyInteger {
-//   public void getThree(int x) {
-//     System.out.println(x);
-//     System.out.println(x);
-//     System.out.println(x);
-//   }
-// }
-
-// 上記と同じようなことをDoubleでもStringでもできるように型を汎用化させる
-
-class MyData<T> {     // <>内に汎用化する方をどう表現するか書く     // T -> Typeという意味で使われる
-  public void getThree(T x) {     // (int x)を(T x)へ変更
-    System.out.println(x);
-    System.out.println(x);
-    System.out.println(x);
+// Runnable   インターフェースの一つ    ここからnew Thread()に処理を渡す
+class MyRunnable implements Runnable {      // implements -> クラスを継承する時,  extends -> インターフェースを適合させる時
+  @Override
+  public void run() {
+    for (int i = 0; i < 500; i++) {
+      System.out.print('*');     // printのみだと改行抜きで表示される   // "" → 文字列　'' → 文字
+    }
   }
 }
 
 public class MyApp {
   
-  public static void main(String[] args) {
-    // MyInteger mi = new MyInteger();
-    // mi.getThree(55);
-  
-    MyData<Integer> i = new MyData<>();     // <Integer>の<>内に入るのは参照型のみ -> int等は不可     // 右辺の変数MyDataの後にも<>をつける
-    i.getThree(32);     // Integer型でMyDataメソッドが使える
-    MyData<String> s = new MyData<>();
-    s.getThree("hello");      // String型でMyDataメソッドが
+  public static void main(String[] args) {      // main()の処理もThread上で実行されている
+    MyRunnable r = new MyRunnable();
+    Thread t = new Thread(r);      // Threadクラスのインスタンスを作成
+    t.start();      // startメソッドを呼び出す -> run()メソッドが実行される
+    
+    
+    for (int i = 0; i < 500; i++) {
+      System.out.print('.');     // printのみだと改行抜きで表示される   // "" → 文字列　'' → 文字
+    }
   }
-
 }

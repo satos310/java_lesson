127031a606b5e0c173a865f29a3625dbf04863cb
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-09 00:56:07 +0000

    [study]無名クラス, ラムダ式, 仮数型インターフェース, #簡略化

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,25 +1,45 @@
 // Thread   コンピュータの処理単位のこと    複数立ち上げれば複数の処理を同時に実行可
 
 // Runnable   インターフェースの一つ    ここからnew Thread()に処理を渡す
-class MyRunnable implements Runnable {      // implements -> クラスを継承する時,  extends -> インターフェースを適合させる時
-  @Override
-  public void run() {
+// class MyRunnable implements Runnable {     // 関数型インターフェース   抽象メソッドを一つだけ持つインターフェース -> 「ラムダ式」という記法に置き換え可
+//   @Override
+//   public void run() {
+//     for (int i = 0; i < 500; i++) {
+//       System.out.print('*');
+//     }
+//   }
+// }
+
+// ↑ 上記クラスは ↓ 下記のnew Threadメソッド内に移動
+
+public class MyApp {
+  
+  public static void main(String[] args) {
+    MyRunnable r = new MyRunnable();
+    // Thread t = new Thread(r);
+    // t.start();
+    
+    // ラムダ式
+    // （引数） -> ｛処理｝   「ラムダ式の記法 == Runnableインターフェース -> run()メソッド」とコンパイラが推測するため、記述不要となる
+    // new Thread(new Runnable() {     // 無名クラス   class名を省略可    // new Rannableには引数がなく処理がfor文のため、voidメソッドは不要
+    //   @Override
+    //   public void run() {
+    //     for (int i = 0; i < 500; i++) {
+    //       System.out.print('*');
+    //     }
+    //   }
+    // }).start();     // インスタンス化後に呼び出している
+    
+    new Thread(() -> {      // ラムダ式の記法     new Threadの引数が{}の中身の処理という意味
+      for (int i = 0; i < 500; i++) {
+        System.out.print('*');
+      }
+    }).start();
+    
     for (int i = 0; i < 500; i++) {
-      System.out.print('*');     // printのみだと改行抜きで表示される   // "" → 文字列　'' → 文字
+      System.out.print('.');
     }
   }
 }
 
-public class MyApp {
-  
-  public static void main(String[] args) {      // main()の処理もThread上で実行されている
-    MyRunnable r = new MyRunnable();
-    Thread t = new Thread(r);      // Threadクラスのインスタンスを作成
-    t.start();      // startメソッドを呼び出す -> run()メソッドが実行される
-    
-    
-    for (int i = 0; i < 500; i++) {
-      System.out.print('.');     // printのみだと改行抜きで表示される   // "" → 文字列　'' → 文字
-    }
-  }
-}
+// Q.「インスタンス化」「インターフェース」「Runnnableインターフェース」「runメソッド」

f9951382fa0f255ef230a0378285ecde9b6e72fb
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-11 22:36:47 +0000

    [study]HashMap, 配列,TreeMap, LinkedHashMap

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,32 +1,33 @@
-// HashSetクラス
+// HashMapクラス
 
 import java.util.*;     // HashSetを使用するためのjava.utilパッケージ
 
 public class MyApp {
 
   public static void main(String[] args) {
-    // HashSet    同様複数のデータを扱う(ArrayListと同じ)   // ArrayListとの相違点・重複を許さない　・データを保持する順番が定まらない（ランダムではない）
-    // TreeSet    値順にソートされる
-    // LinkedHashSet    追加された順を保持する
-    
-    // HashSet<Integer> sales = new HashSet<>();
-    Set<Integer> sales = new HashSet<>();     // ArrayListと同様にSetインターフェース型で宣言可
-    
-    sales.add(10);
-    sales.add(20);
-    sales.add(30);
-    sales.add(10);      // 同じ値を追加しても、「重複を許さない」-> 要素は増えない, 「順番が定まらない」-> 『○番目を取り出す』ができない
-    
-    System.out.println(sales.size());     // sizeメソッドで要素の個数を取得することはできる   // 3
-    
-    for (Integer sale : sales) {      // for文を使用して要素の中身全てを取り出す
-      System.out.println(sale);      // 取り出す順番は保証されない
+    // HashMap    複数データを管理するためのもの    keyとvalueでデータを管理    // 追加したデータの順番が不定
+    // TreeMap    keyの順番でデータを保持
+    // LinkedHashMap    データが追加された順番でデータを保持
+
+    // HashMap<String, Integer> sales = new HashMap<>();      // key: Stringとvalue: valueの型を指定
+    Map<String, Integer> sales = new HashMap<>();     // HashSet : SetのようにHashMap: Map型のインターフェースで宣言可
+
+    sales.put("tom", 10);
+    sales.put("bob", 20);
+    sales.put("steve", 30);
+
+    System.out.println(sales.get("tom"));     // get() -> 要素の取得, key(tom)の取得   // vakue（値）は10
+    System.out.println(sales.size());     // size() -> 要素の個数も取得できる -> 3
+
+    for (Map.Entry<String, Integer> sale : sales.entrySet()) {     // Map.Entry: 全ての中身を確認, entrySet: <Integer, String>の組み合わせを一つずつ扱うループ
+      System.out.println(sale.getKey() + ":" + sale.getValue());
     }
-    
-    sales.remove(30);     // 要素の削除 -> 直接選択して削除
-    
-    for (Integer sale : sales) {      // for文を使用して要素の中身全てを取り出す
-      System.out.println(sale);      // 取り出す順番は保証されない
+
+    sales.put("tom", 100);     // put: 値の更新
+    sales.remove("steave");      // 要素の削除
+
+    for (Map.Entry<String, Integer> sale : sales.entrySet()) {
+      System.out.println(sale.getKey() + ":" + sale.getValue());
     }
   }
 }

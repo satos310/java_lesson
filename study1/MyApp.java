b8f17dd1dd7d6968d3abb78e2abc911c95379be3
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-16 00:41:27 +0000

    [制御構造4]さらに条件を追加, else if, 条件判定の順序

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,5 +1,5 @@
 // ユーザーから入力を受け取る
-// if文で条件分岐
+// if文で条件分岐   比較演算子
 
 import java.util.Scanner;
 
@@ -7,10 +7,20 @@ public class MyApp {
   public static void main(String[] args) {
     System.out.print("Score? ");     // 
     int score = new Scanner (System.in).nextInt();     // 
+    // if (score >= 90) {
+    //   System.out.println("A!");
+    // } else if (score >= 70) {     // 条件を上から見てる -> 条件を満たす -> それ以降の条件については判定しない
+    //   System.out.println("Not B!");
+    // } else {
+    //   System.out.println("Not C!");
+    // }
+    
     if (score >= 90) {
-      System.out.println("A!");
+      System.out.println("A!");     // A!の条件に当てはまっている
+    } else if (score >= 95) {
+      System.out.println("super!!");      // else ifは実行されない
     } else {
-      System.out.println("Not A!");
+      System.out.println("Not C!");
     }
   }
 }

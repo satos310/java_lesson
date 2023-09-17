7cbde69372aa252aea47e0c1274c4bc0a318ece1
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-17 15:42:56 +0000

    [制御編5]条件分岐で文字列を扱う, equals(), 真偽値による判定

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,26 +1,21 @@
 // ユーザーから入力を受け取る
 // if文で条件分岐   比較演算子
+// 条件分岐で文字列を扱う   // 信号機の色を入れたら、それに対応するメッセージの表示
 
 import java.util.Scanner;
 
 public class MyApp {
   public static void main(String[] args) {
-    System.out.print("Score? ");     // 
-    int score = new Scanner (System.in).nextInt();     // 
-    // if (score >= 90) {
-    //   System.out.println("A!");
-    // } else if (score >= 70) {     // 条件を上から見てる -> 条件を満たす -> それ以降の条件については判定しない
-    //   System.out.println("Not B!");
-    // } else {
-    //   System.out.println("Not C!");
-    // }
+    System.out.print("Signal? ");     // 
+    String signal = new Scanner (System.in).next();     // 文字列を受け取りたい -> next,  数字を受け取りたい -> nextInt
     
-    if (score >= 90) {
-      System.out.println("A!");     // A!の条件に当てはまっている
-    } else if (score >= 95) {
-      System.out.println("super!!");      // else ifは実行されない
+    // if (signal == "red") {     // この形式でも物理的に「色（文字列）」を判別することはできる
+    // }
+
+    if (signal.equals("red") == true) {     // 変数.equals(比較したい文字列) -> 「文字列が同じかどうか比較する際に使う特別な命令
+      System.out.print("Stop");
     } else {
-      System.out.println("Not C!");
+      System.out.println("Not red");
     }
   }
 }

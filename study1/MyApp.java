cf08496ed7e93e4d700a34f3aee4f7f31e08ece2
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-17 22:20:45 +0000

    [制御構造6]switch文で条件分岐, case, break, default

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,6 +1,7 @@
 // ユーザーから入力を受け取る
 // if文で条件分岐   比較演算子
 // 条件分岐で文字列を扱う   // 信号機の色を入れたら、それに対応するメッセージの表示
+// switch文で条件分岐     // red, blue , yellowのように取り得る値が決まっている場合、if...elseよりもすっきり書ける場合もある
 
 import java.util.Scanner;
 
@@ -9,13 +10,23 @@ public class MyApp {
     System.out.print("Signal? ");     // 
     String signal = new Scanner (System.in).next();     // 文字列を受け取りたい -> next,  数字を受け取りたい -> nextInt
     
-    // if (signal == "red") {     // この形式でも物理的に「色（文字列）」を判別することはできる
-    // }
-
-    if (signal.equals("red") == true) {     // 変数.equals(比較したい文字列) -> 「文字列が同じかどうか比較する際に使う特別な命令
-      System.out.print("Stop");
-    } else {
-      System.out.println("Not red");
+    switch (signal) {     // signalの取りうる値（変数の中身）に対しての処理
+      case "red":
+        System.out.println("Stop");
+        break;
+        
+      case "yellow":
+        System.out.println("Slow down");
+        break;
+        
+      case "blue":
+      case "green":     // 条件に"又は"を作りたいときは、caseを縦に並べるだけ
+        System.out.println("Go");     // "blue"または"green"の時にGoと表示させる
+        break;
+        
+      default:      // どのcaseにも当てはまらない場合（if文でいうelseのようなもの）
+        System.out.println("Wong signal");
+        break;
     }
   }
 }

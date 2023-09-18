adea45546e6bc156b10093490aa5e8266de55e0a
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-18 14:55:08 +0000

    [制御構造8]for文, 初期値・条件の変更

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,32 +1,15 @@
-// ユーザーから入力を受け取る
-// if文で条件分岐   比較演算子
-// 条件分岐で文字列を扱う   // 信号機の色を入れたら、それに対応するメッセージの表示
-// switch文で条件分岐     // red, blue , yellowのように取り得る値が決まっている場合、if...elseよりもすっきり書ける場合もある
+// for文
+// Helloを３回表示させる
 
 import java.util.Scanner;
 
 public class MyApp {
   public static void main(String[] args) {
-    System.out.print("Signal? ");     // 
-    String signal = new Scanner (System.in).next();     // 文字列を受け取りたい -> next,  数字を受け取りたい -> nextInt
-    
-    switch (signal) {     // signalの取りうる値（変数の中身）に対しての処理
-      case "red":
-        System.out.println("Stop");
-        break;
-        
-      case "yellow":
-        System.out.println("Slow down");
-        break;
-        
-      case "blue":
-      case "green":     // 条件に"又は"を作りたいときは、caseを縦に並べるだけ
-        System.out.println("Go");     // "blue"または"green"の時にGoと表示させる
-        break;
-        
-      default:      // どのcaseにも当てはまらない場合（if文でいうelseのようなもの）
-        System.out.println("Wong signal");
-        break;
+    // for(int i = 0; i < 3; i++) {      // ①   下記①〜③は全て同じ結果    単純に指定した回数だけ処理を行いたい場合によく使われる
+    // for(int i = 1; i < 4; i++) {      // ②   初期値は0でなくてもOK
+    for(int i = 1; i <= 3; i++) {      // ③
+      System.out.println(i + " Hello");     // "Hello"の前にi(変数)を持ってくることで、回数を表示させる
     }
+    
   }
 }

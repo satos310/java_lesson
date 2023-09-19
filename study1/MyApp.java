79ce9d26fbc3997966df8f18ca564235e821d04b
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-19 22:32:59 +0000

    [制御構造11]while, 繰り返す回数が不明瞭な場合に使用

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,18 +1,15 @@
-// 拡張for文を使ってみる
-// 単に配列の全ての要素に対して何らかの処理をしたい場合に、すっきりと書くことができる
+// while文で反復処理をする
+// 繰り返し回数が予あらかじめ決まっていない、分かりにくい場合に使用する
 
 import java.util.Scanner;
 
+// 500円のお金を1年毎に10％ずつ増えて言った場合の条件・反復処理
 public class MyApp {
   public static void main(String[] args) {
-    int [] scores = {70, 90, 80, 60};
-    
-    // for (int i = 0; i < scores.length; i++) {      // 前章の記述方法   メリット：ブロックの中で i の値を使えるため、それを使った処理を組み立て可
-    //   System.out.println(scores[i]);
-    // }
-    
-    for(int score: scores) {      // 拡張for文    for(変数の宣言: 配列の変数名, ブロック)
-      System.out.println(score);      // scoreの要素がなくなるまで、各要素を１つずつ変数にセットされていく    ブロックの中の処理を実行
+    double money = 500;
+    while (money < 1000.0) {      // moneyが１０００円を超えるまで反復処理を実行
+      money *= 1.1;
+      System.out. println(money);
     }
   }
 }

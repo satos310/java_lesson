9e2f2409b0fe47305c65077696fd5018c943c4ed
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-19 11:49:39 +0000

    [制御構造10]拡張for文, for文との違い, for(変数の宣言: 配列の変数名)

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,18 +1,18 @@
-// 配列と反復処理   よく組み合わせで使われる
-// 
+// 拡張for文を使ってみる
+// 単に配列の全ての要素に対して何らかの処理をしたい場合に、すっきりと書くことができる
 
 import java.util.Scanner;
 
 public class MyApp {
   public static void main(String[] args) {
-    int [] scores = {70, 90, 80, 60};      // 整数の配列    }のあとは;必要
-    // System.out.println(scores[0]);
-    // System.out.println(scores[1]);
-    // System.out.println(scores[2]);      // ▲要素が多くなると、コードが長くなる
+    int [] scores = {70, 90, 80, 60};
     
-    // for (int i = 0; i < 3; i++) {
-    for (int i = 0; i < scores.length; i++) {     // 変数.lengthとすることで、配列の要素の数だけ実行する
-      System.out.println(scores[i]);      // scoresの配列Noを[i]に代入
+    // for (int i = 0; i < scores.length; i++) {      // 前章の記述方法   メリット：ブロックの中で i の値を使えるため、それを使った処理を組み立て可
+    //   System.out.println(scores[i]);
+    // }
+    
+    for(int score: scores) {      // 拡張for文    for(変数の宣言: 配列の変数名, ブロック)
+      System.out.println(score);      // scoreの要素がなくなるまで、各要素を１つずつ変数にセットされていく    ブロックの中の処理を実行
     }
   }
 }

d6a1bdc00b64681ffc1828b3539b31a24b895620
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-22 07:39:40 +0000

    [制御構造16]scope, if{} for{}におけるスコープ

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,20 +1,25 @@
-// 論理演算子
-// AなおかつB -> A && B,    AまたはB -> A || B,   Aではない -> !A
+// スコープについて
+// 変数や定数が有効である範囲のこと
+// スコープが異なれば同じ名前で変数を宣言することができる(変数の使い回し可)
 
 import java.util.Scanner;
 
 // EnglishとMathの点数を受け取り、その値によって処理を条件分岐させる
 public class MyApp {
-  public static void main(String[] args) {
-    System.out.print("English? ");
-    int english = new Scanner(System.in).nextInt();
-    System.out.print("Math? ");
-    int math = new Scanner(System.in).nextInt();
+  // public static void main(String[] args) {
+  //   int amount = 120;
     
-    if (!(english >= 80 && math >= 80)) {     // !を頭につけることで、動作が逆になり「どちらも80点以下の時」にA!と表示される
-      System.out.println("A!");
-    } else {
-      System.out.println("B!");
+  //   if (amount > 0) {
+  //     int sales = 100 * amount;     // 変数は宣言されたブロックの中の宣言された箇所以降(以下)で有効　 Javaのルール
+  //     System.out.println(sales);
+  //   }
+    
+    // System.out.println(sales);     // 宣言されたブロック外になるため、salesは使えない
+    
+    for (let i = 0; i < 3; i++) {     // 変数iのスコープはfor{}内になる
+      System.out.println(i);
     }
+    
+    // System.out.println(i);     // iは宣言箇所から見てブロック外になるため、使用不可
   }
 }

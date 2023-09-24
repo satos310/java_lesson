1b928a13d8fc09b66a98059b5746cb25d4ac0213
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-24 12:01:30 +0000

    [メソッド4]sum()...返り値あり, showSum()...返り値なし, void...何も返さない

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,15 +1,18 @@
-// メソッドの基本的な構造は「入力(引数) -> 処理 -> 出力(返り値)」
-// 仮引数...「仮で変数を使っておく」  メソッドを定義している際に渡している
-// 実引数...「実際にこの値を使って」  メソッドを実行する際に渡している
+// 返り値のないメソッドを実装
 
-// 
+// 2つの整数値を渡す -> 合計して返してくれる    sumメソッド
 public class MyApp {
-  private static int triple(int num) {     // 仮引数     // private static...フィールド：クラス内からのみアクセス可能、　メソッド：クラス内からのみ呼び出し可能
-    return num * 3;     // tripleメソッドの処理の中身
+  private static int sum(int a, int b) {      // 1.返り値があるメソッド -> 返り値の結果を使って他の演算ができる
+    return a + b;
+  }
+
+  private static void showSum(int a, int b) {     // 2.showSumメソッド -> 合計を表示するだけであれば直接指示可
+                                                  // returnがないため、int -> void（何も返さない）にする必要有
+    System.out.println(a + b);
   }
   
   public static void main(String[] args) {
-    System.out.println(triple(10));     // 実引数 30   // triple...整数値を３倍にする
-    System.out.println(triple(4) * 3);      // 実引数 36
+    System.out.println(sum(3, 7));
+    showSum(3, 7);
   }
 }

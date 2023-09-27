dd8f9580c6ea9b36394cf973f51f4b8ac4e96d8d
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-27 03:03:43 +0000

    [メソッド9]メソッドのオーバーロード, シグネチャ, showThreeTimes()

--- study1/MyApp.java
+++ study1/MyApp.java
@@ -1,14 +1,25 @@
-// 仮引数のスコープ
+// メソッドをオーバーロードする
+// ある名前のメソッドを違う（複数の）データ型の引数に対応させていくことができる
 
-// 整数値を3倍に返す
+// メソッド名、引数の数、型、順序の組み合わせをメソッドのシグネチャーと呼ぶ。
+
+// 整数を渡すと、それを3回表示してくれる
 public class MyApp {
-  private static int triple(int num) {
-    return num * 3;
+  private static void showThreeTimes(int n) {     // 整数 が渡されたらこのprovateフィールド内のメソッドが実行される
+    System.out.println(n);
+    System.out.println(n);
+    System.out.println(n);
+  }
+  
+  // 「メソッドをオーバーロードする」...同じ名前のメソッドを違うシグネチャーで定義すること
+  private static void showThreeTimes(double n) {      // 小数 が渡されたらこのprovateフィールド内のメソッドが実行される
+    System.out.println(n);
+    System.out.println(n);
+    System.out.println(n);
   }
   
   public static void main(String[] args) {
-    int num = 100;
-    System.out.println(triple(10));
-    System.out.println(num);      // 100    // privateフィールド（スコープ）のnumとは違う変数として扱われる
+    showThreeTimes(3);
+    showThreeTimes(5.2);
   }
 }

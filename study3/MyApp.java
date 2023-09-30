75f3422a3a796946a053c9af6235b630aacad1ff
satos310 <119788681+satos310@users.noreply.github.com>
2023-09-30 12:17:35 +0000

    [クラス5]クラスにメソッドを定義する, getUserString(), thisの省略

--- study3/MyApp.java
+++ study3/MyApp.java
@@ -1,34 +1,29 @@
-// コンストラクタを使用する
+// クラスにメソッドを定義する
 
-// 
+//
 class User {
   String name;
   int score;
-  
-  // constructor
-  // User(String nameFromNew, int scoreFromNew) {     // コンストラクタの仮引数
-  //   this.name = nameFromNew;     // this.  このクラスからインスタンスが作られていることを明示
-  //   this.score = scoreFromNew;
-  // }
+
   User(String name, int score) {      // 仮引数の名前をフィールドと同じにすることも可
     this.name = name;     // インスタンスのフィールドにはthis.が付いてることで区別する
     this.score = score;
+  }
+
+  String getUserString() {
+    // return this.name + ", " + this.score;     // 変数を使ってコードを省略
+    return name + ", " + score;     // 仮引数がないため、this.を省略しても動作する()
   }
 }
 
 public class MyApp {
   public static void main(String[] args) {
     User user1 = new User("Taro", 70);      // 新しくインスタンスを作成する際に値を設定 -> 下記の値の設定が不要
-    // user1.name = "Taro";      // user1のnameフィールドに値を設定したい
-    // user1.score = 70;
-    
+
     User user2 = new User("Jiro", 80);
-    // user2.name = "Jiro";      // user2のnameフィールドに値を設定したい
-    // user2.score = 80;
-    
-    System.out.println(user1.name);
-    System.out.println(user1.score);
-    System.out.println(user2.name);
-    System.out.println(user2.score);
+
+    System.out.println(user1.getUserString());
+    System.out.println(user2.getUserString());
+
   }
 }

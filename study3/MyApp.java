7f5f8c493484feb7a5c04447df6728a4e745c5d8
satos310 <119788681+satos310@users.noreply.github.com>
2023-10-01 12:55:09 +0000

    [クラス7]アクセス修飾子(private)で想定外の値を入れないようにする, カプセル化, getScore(), setter/getter

--- study3/MyApp.java
+++ study3/MyApp.java
@@ -1,9 +1,10 @@
-// メソッドを介して値を変更する
+// アクセス修飾子を使う
+// 想定外の値を入れない安全なプログラム
 
-//
+// 0~100点から外れた数字をエラーで弾く
 class User {
   String name;
-  int score;
+  private int score;        // カプセル化   scoreに対してprivateというキーワードをつける -> Userクラスの外からはScoreフィールドにアクセス・取得できなくなる（アクセス修飾子）
 
   User(String name, int score) {
     this.name = name;
@@ -11,9 +12,10 @@ class User {
   }
 
   String getUserString() {
-    return name + ", " + score; 
+    return name + ", " + score;
   }
   
+  // setter   フィールドに値をセットするメソッド
   void setScore(int score) {
     if (score < 0 || score > 100) {
       System.out.println("Invalid score!");
@@ -21,15 +23,23 @@ class User {
     }
     this.score = score;
   }
+  
+  // getter   値を取得するためのメソッド
+  // よく使われるテクニック   安全にアクセスするためのメソッドを別途用意する
+  int getScore() {      // privateにしたフィールドの値を取得したい -> メソッドを定義する
+    return this.score;      // privateのscoreに対して、インスタンスのscoreを返す    // 返り値はint
+  }
 }
 
 public class MyApp {
   public static void main(String[] args) {
     User user1 = new User("Taro", 70);
     User user2 = new User("Jiro", 80);
-    
+
     // user1.score = 900;      // 謝って入力ミスが起こしてしまう
-    user1.setScore(900);
+    // user1.setScore(900);
+    // System.out.println(user1.score);
+    System.out.println(user1.getScore());
 
     System.out.println(user1.getUserString());
     System.out.println(user2.getUserString());

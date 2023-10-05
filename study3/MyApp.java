2a5ada7b92f5c87df8f6fe7d8c7f71948a180f16
satos310 <119788681+satos310@users.noreply.github.com>
2023-10-05 12:58:52 +0000

    [クラス12]インスタンスの情報を表示する, クラスごとに実装の役割を分ける

--- study3/MyApp.java
+++ study3/MyApp.java
@@ -1,6 +1,7 @@
-// Scoreクラスを定義する
+// インスタンスの情報を表示してみる
+// クラスごとに「Scoreに関する実装」「Userに関する実装」を分ける -> 機能拡張時に修正しやすい
 
-// 点数に科目の内容も含める -> Scoreというデータ型を作りプログラムを拡張する
+// MyAppクラス内のインスタンスに設定された情報を表示する
 class Score {
   private String subject;
   private int score;
@@ -8,25 +9,33 @@ class Score {
   Score(String subject, int score) {
     this.subject = subject;
     this.score = score;     // 宣言の理由を復習要
-    
+  }
+  
+  String getScoreString() {     // 3. private外でsubject, scoreのデータを引数にするメソッドを定義
+    return this.subject + ", " + this.score;
   }
 }
 
 class User {
   private String name;
-  private Score score;      // Q. ここも
-  // A. new Score()でStringとintを一塊で扱っているため、変数のような箱で一括にデータを送り返ししている
-  User(String name, Score score) {      // Q. なぜUserクラスのコンストラクタに渡すsocoreの値がintからScoreになるのか？
+  private Score score;
+  User(String name, Score score) {
     this.name = name;
     this.score = score;
+  }
+  
+  String getString() {
+    // return this.name + ", " + this.score.subject;      // 1. 文字列を返したい but scoreとsubjectはScoreクラスでprivateに設定されている
+    return this.name + ", " + this.getScoreString();      // 2. getScoreString()というメソッドを実行し、結果を連結する
   }
 }
 
 public class MyApp {
   public static void main(String[] args) {
-    // 点数 + 科目の内容を含める -> データ型(Score)を作ってプログラムを拡張する
     User user1 = new User("Taro", new Score("Math", 70));     // Scoreクラスでは科目と点数を保持したい -> 数学, 70と明記
     User user2 = new User("Jiro", new Score("English", 80));
-
+    
+    System.out.println(user1.getString());
+    System.out.println(user2.getString());
   }
 }

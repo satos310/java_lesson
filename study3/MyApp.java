e56249b25bbfeb5611a7b60f3a5e26450e2186e2
satos310 <119788681+satos310@users.noreply.github.com>
2023-10-08 11:13:03 +0000

    [クラス16]メソッドのオーバーライド, 親クラスのメソッドを小クラスで上書き, @Override, protected

--- study3/MyApp.java
+++ study3/MyApp.java
@@ -1,35 +1,51 @@
-// Scoreクラスを継承する
+// メソッドをオーバーライドする
 
-// 処理を小クラスに分ける -> 親クラスと同じように動作するため、コードの変更が少なくて済む
+// ＠Override
+// private -> protected   親クラスのメソッドを小クラスで上書き
 class Score {
   private String subject;
-  private int score;
-  
+  protected int score;
+
   Score(String subject, int score) {
     this.subject = subject;
     this.score = score;
   }
-  
-  private String getResult() {
+
+  protected String getResult() {      // private -> protected   小クラス(教科ごと)でgetResult()を上書きしたい
     return this.score >= 80? "Pass" : "Fails";
   }
-  
+
   String getScoreString() {
     return this.subject + ", " + this.score + ", " +this.getResult();
   }
 }
 
+
 class MathScore extends Score {     // クラス名 extends 継承元クラス名
-  MathScore (int score) {     // 引数の数を変えた -> それぞれのクラスでコンストラクターを再定義する必要がある
-    // 親クラスのコンストラクタと同じような処理をしたい
+  MathScore (int score) {
     super("Math", score);     // super()    親クラスのコンストラクタを
   }
+
+  @Override     // メソッドのオーバーライド(アノテーション)    小クラス内で親クラスと同じ名前のメソッドを上書きする -> 親クラスよりも優先して判定される
+  protected String getResult() {      // private -> protected   小クラスでgetResult()を上書きしたい
+    System.out.println("MathScore method");
+    return this.score >= 50? "Pass" : "Fails";
+  }
 }
+
+
 class EnglishScore extends Score {     // クラス名 extends 継承元クラス名
   EnglishScore (int score) {
     super("English", score);
   }
+
+  @Override
+  protected String getResult() {
+    System.out.println("EnglishScore method");
+    return this.score >= 70? "Pass" : "Fails";
+  }
 }
+
 
 class User {
   private String name;
@@ -38,7 +54,7 @@ class User {
     this.name = name;
     this.score = score;
   }
-  
+
   String getUserString() {
     return this.name + ", " + this.score.getScoreString();
   }
@@ -46,9 +62,9 @@ class User {
 
 public class MyApp {
   public static void main(String[] args) {
-    User user1 = new User("Taro", new MathScore(70));     // 新しく作ったクラス名のインスタンス作成
-    User user2 = new User("Jiro", new EnglishScore(80));     // 新しく作ったクラス名のインスタンス作成
-    
+    User user1 = new User("Taro", new MathScore(70));
+    User user2 = new User("Jiro", new EnglishScore(80));
+
     System.out.println(user1.getUserString());
     System.out.println(user2.getUserString());
   }
